9522cb98dd25caf550aad06c6fc823f011eedcac
Madhu <114853422+MadhuSuniL@users.noreply.github.com>
2024-04-02 20:45:10 +0530

    Update settings.py

--- plutogptbe/settings.py
+++ plutogptbe/settings.py
@@ -1,8 +1,9 @@
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'plutogptbe.settings')
-
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
+django.setup()
 from channels.security.websocket import AllowedHostsOriginValidator
 from plutogptbe.urls import ws_urlpatterns
 from Users.ws_auth_middleware import WsAuthMiddleware
